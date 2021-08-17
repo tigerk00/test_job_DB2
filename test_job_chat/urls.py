5b0cb3f9d80d54e12ad74df245cc5418dc334b06
tigerk00 <kostia.lagoda@gmail.com>
2021-08-17 22:19:59 +0300

    final cosmetic job

--- test_job_chat/urls.py
+++ test_job_chat/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Chat_B2 API",
+        title="Chat_DB2 API",
         default_version="v1",
         description="A simple API to view and post messages without registering.",
         terms_of_service="https://www.google.com/policies/terms/",
