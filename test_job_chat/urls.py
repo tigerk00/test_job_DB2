e2f5cb0f49fa235a1cbc09932a02b715a7d1c91a
tigerk00 <kostia.lagoda@gmail.com>
2021-08-17 22:14:53 +0300

    setting swagger as start page -_-

--- test_job_chat/urls.py
+++ test_job_chat/urls.py
@@ -43,6 +43,5 @@ admin.site.enable_nav_sidebar = False
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/messages/', include('chat.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
