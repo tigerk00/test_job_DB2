6bbe8461ebe4fbb507b7bd51c14a19094112f081
tigerk00 <kostia.lagoda@gmail.com>
2021-08-17 22:00:02 +0300

    added alowed host heroku

--- test_job_chat/settings.py
+++ test_job_chat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','blooming-lake-48999.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','testchattigerk.herokuapp.com']
 
 
 # Application definition
